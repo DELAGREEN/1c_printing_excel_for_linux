e9cd3eca177fa600d0bb05d7a3aac0689cb966ec
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2023-11-30 19:09:38 +0300

    formatting date and time in the logging

--- errors_logger.py
+++ errors_logger.py
@@ -13,7 +13,8 @@ class ErrorsLogger():
             logging.basicConfig(level=logging.INFO,
                             filename="tests\excel_printer.log",
                             filemode="a+",
-                            format="%(asctime)s %(levelname)s %(message)s")
+                            format="%(asctime)s %(levelname)s %(message)s",
+                            datefmt="%d-%m-%Y %H:%M:%S")
             return func(*args, **kwgs)
         return printer
     
