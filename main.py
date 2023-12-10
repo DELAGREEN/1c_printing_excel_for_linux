f79fb1db8a0d737be3ad7fa84a9117425119ab87
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2023-12-10 15:17:49 +0300

    from glibc2.27 :"(

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import sys
 from openpyxl.workbook import Workbook
 from openpyxl.reader.excel import load_workbook
-import time
+#import time
 import os
 from errors_logger import ErrorsLogger as Logger
 #from icecream import ic
@@ -68,7 +68,7 @@ def read_txt(_path_to_txt:str):
 def writer_for_excel(_strings, path_to_excel:str, separator_first:str, separator_end:str):
 
     #дробим строку вида [номер строки, номер столбца, значение ячейки][разделитель][номер строки, номер столбца, значение ячейки]
-    list_strings = _strings.split(separator_end)                             #и получаем [номер строки][разделитель][номер столбца][разделитель][значение ячейки]                     
+    list_strings = _strings.split(separator_end)                            #и получаем [номер строки][разделитель][номер столбца][разделитель][значение ячейки]                     
     
     '''Удаляем задолбавший \n в конце строки'''
     '''Требует ОСОБОГО ВНИМАНИЯ'''
@@ -77,27 +77,28 @@ def writer_for_excel(_strings, path_to_excel:str, separator_first:str, separator
         list_strings[-1] = list_strings[-1][:-1]
     
     '''Требует Особого Внимания'''
-    wb = load_workbook(path_to_excel)                                        #Передаем классу, методу класса Файл который нужно открыть
+    wb = load_workbook(path_to_excel)                                       #Передаем классу, методу класса Файл который нужно открыть
     
     for i in list_strings:
         
-        cell_data_set = i.split(separator_first)                             #дробим строку на [номер строки][номер столбца][значение ячейки]
-
-        match cell_data_set:
-            case _row, _column, _value:                                      #дробим строку на [номер строки][номер столбца][значение ячейки] 
-                #_row, _column, _value = cell_data_set                       #Эта строка аналогична верхней, более читабельна, оставил что бы не забыть как это выглядит по нормальному
-                ws = wb.worksheets[0]                                        #Номер страницы для записи
-                #ws[column] = value                                          #colunm должен равняться номеру столбца НАПРИМЕР: А1
-                ws.cell(row=int(_row), column=int(_column)).value = _value   #передаем текст в ячейку 
+        cell_data_set = i.split(separator_first)                            #дробим строку на [номер строки][номер столбца][значение ячейки]
+        len_arr_cell = len(cell_data_set)
+        if len_arr_cell == 3:
+            _row, _column, _value = cell_data_set                           #дробим строку на [номер строки][номер столбца][значение ячейки] 
+            #_row, _column, _value = cell_data_set                          #Эта строка аналогична верхней, более читабельна, оставил что бы не забыть как это выглядит по нормальному
+            ws = wb.worksheets[0]                                           #Номер страницы для записи
+            #ws[column] = value                                             #colunm должен равняться номеру столбца НАПРИМЕР: А1
+            ws.cell(row=int(_row), column=int(_column)).value = _value      #передаем текст в ячейку 
         
-            case _row, _column, _value, _sheet_num:                          #дробим строку на [номер строки][номер столбца][значение ячейки][номер страницы]
-                #_row, _column, _value, _sheet_num = cell_data_set             
-                ws = wb.worksheets[int(_sheet_num)]                          #Номер страницы для записи
-                ws.cell(row=int(_row), column=int(_column)).value = _value   #передаем текст в ячейку
+        elif len_arr_cell == 4:
+            _row, _column, _value, _sheet_num = cell_data_set               #дробим строку на [номер строки][номер столбца][значение ячейки][номер страницы]
+            #_row, _column, _value, _sheet_num = cell_data_set             
+            ws = wb.worksheets[int(_sheet_num)]                             #Номер страницы для записи
+            ws.cell(row=int(_row), column=int(_column)).value = _value      #передаем текст в ячейку
 
-            case _:
-                Logger.print_error('Не правильно сереализирован TXT файл.')
-                print('Не правильно сереализирован TXT файл.')
+        else:
+            Logger.print_error('Не правильно сереализирован TXT файл.')
+            print('Не правильно сереализирован TXT файл.')
 
     wb.save(path_to_excel)                                                   #сохраняем файл
     Logger.print_info('Write is сomplite!')
@@ -112,8 +113,7 @@ def parse_param():
     separator_end = 'None'
 
     param_name = sys.argv[1]
-    match param_name:
-        case '--excel'| '-excel':                                            #Если есть тригер выполняем код ниже           
+    if param_name == '--excel'| '-excel':                                    #Если есть тригер выполняем код ниже                                                    
             try:
                 path_to_excel = str(sys.argv[2])                             #Захватываем путь к excel
                 path_to_txt = str(sys.argv[3])                               #Захватываем путь к txt
@@ -143,41 +143,41 @@ def parse_param():
                                     f'EXCEPTION: {_ex}') 
                     print(f'{textcolors.RED}Ошибка.{textcolors.YELLOW} Неизвестная ошибка: Проверьте правильность параметра:{param_name}\n{_ex}')
         
-        case '--help'|'-help':
-            print(f'{textcolors.CYAN}***HELP SHEET***\n{doc}')  
+    elif param_name == '--help'|'-help':
+        print(f'{textcolors.CYAN}***HELP SHEET***\n{doc}')  
    
-        case _:
-            Logger.print_error(f'Неизвестный параметр: {param_name}')
-            print(f'{textcolors.RED}Ошибка. {textcolors.YELLOW}Неизвестный параметр: {param_name}')
+    else:
+        Logger.print_error(f'Неизвестный параметр: {param_name}')
+        print(f'{textcolors.RED}Ошибка. {textcolors.YELLOW}Неизвестный параметр: {param_name}')
             
 
 if __name__ == '__main__':
 
     Logger.print_info('Start programm...')
 
-    match len(sys.argv):
+    input_param= len(sys.argv)
 
-        case 1:
-            print(f'{textcolors.RED}Внимание! {textcolors.YELLOW}Дополнительные параметры не заданы.\n' 
-                'Модуль не работает без дополнительных параметров.\n'
-                f'В качестве параметров принимается {textcolors.GREEN}Строка(String){textcolors.YELLOW} в качестве разделителей параметров {textcolors.GREEN}Пробел(Space).{textcolors.YELLOW} Пример: {textcolors.BLUE}[Параметр1] [Параметр2] [Параметр3]{textcolors.YELLOW} и тд (без квадратных скобок).\n'
-                '\n'
-                'На данный момент принимаются:'
-                f'{param_list}\n'
-                f'{textcolors.YELLOW}*ИЛИ*\n'
-                'Ознакомтесь с документацией c помошью команды -help.'
-                f'{textcolors.END}')
-            Logger.print_warning('Дополнительные параметры не заданы.')
+    if input_param == 1:
+        print(f'{textcolors.RED}Внимание! {textcolors.YELLOW}Дополнительные параметры не заданы.\n' 
+            'Модуль не работает без дополнительных параметров.\n'
+            f'В качестве параметров принимается {textcolors.GREEN}Строка(String){textcolors.YELLOW} в качестве разделителей параметров {textcolors.GREEN}Пробел(Space).{textcolors.YELLOW} Пример: {textcolors.BLUE}[Параметр1] [Параметр2] [Параметр3]{textcolors.YELLOW} и тд (без квадратных скобок).\n'
+            '\n'
+            'На данный момент принимаются:'
+            f'{param_list}\n'
+            f'{textcolors.YELLOW}*ИЛИ*\n'
+            'Ознакомтесь с документацией c помошью команды -help.'
+            f'{textcolors.END}')
+        Logger.print_warning('Дополнительные параметры не заданы.')
     
-        case 2:
-            parse_param()   #Парсим параметры
+    elif input_param == 2:
+        parse_param()   #Парсим параметры
 
-        case 6:
-            parse_param()   #Парсим параметры 
+    elif input_param == 6:
+        parse_param()   #Парсим параметры 
         
-        case _:
-            print(f'{textcolors.RED}Ошибка. {textcolors.YELLOW}Слишком мало параметров. Воспользуйтесь командой -help.')
-            Logger.print_error('Длина массива параметров не соответствует требуемым.')
+    else:
+        print(f'{textcolors.RED}Ошибка. {textcolors.YELLOW}Слишком мало параметров. Воспользуйтесь командой -help.')
+        Logger.print_error('Длина массива параметров не соответствует требуемым.')
 
 Logger.print_info('End programm...')
 sys.exit(1)
